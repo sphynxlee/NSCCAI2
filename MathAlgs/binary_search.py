a0afd67b29c4e1f2029ba1df5d20f0a84fcfd40d
haidun.lhd <kaiton.ri@gmail.com>
2024-01-09 10:46:15 -0400

    Add binary search algorithm implementation

---
+++ MathAlgs/binary_search.py
@@ -0,0 +1,17 @@
+def binary_search (list, target):
+    left = 0
+    right = len(list) - 1
+    while left <= right:
+        midpoint = (left + right) // 2
+        current_item = list[midpoint]
+        if current_item == target:
+            return midpoint
+        elif target < current_item:
+            right = midpoint - 1
+        else:
+            left = midpoint + 1
+
+my_list = [2, 3, 4, 5, 6, 7, 9, 11, 13]
+target_value = 7
+result = binary_search(my_list, target_value)
+print(result)
