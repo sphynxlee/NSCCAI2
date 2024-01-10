2ed81b921dbb2692bb4e4f5d9cc58ff6b95e1840
haidun.lhd <kaiton.ri@gmail.com>
2024-01-10 00:15:30 -0400

    Add KMeans clustering of weather data

--- ML/KMeans.py
+++ ML/KMeans.py
@@ -1,5 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
+from matplotlib.markers import MarkerStyle
 
+np.random.seed(0)
+
+# Create sunny data
+sunny_data = 20 + 10 * np.random.randn(100, 2)
+
+# cloudy data
+cloudy_data = 50 + 10 * np.random.randn(100, 2)
+
+# rainy data
+rainy_data = 80 + 10 * np.random.randn(100, 2)
+
+# snowy data
+snowy_data = 110 + 10 * np.random.randn(100, 2)
+
+# Combine all weather data
+weather_data = np.vstack([sunny_data, cloudy_data, rainy_data, snowy_data])
+
+model = KMeans(n_clusters=4, random_state=42)
+model.fit(weather_data)
+
+# Get cluster centers and labels
+centers = model.cluster_centers_
+labels = model.labels_
+
+# Plotting the data points and cluster centers
+plt.scatter(weather_data[:, 0], weather_data[:, 1], c=labels)
+plt.title('KMeans Clustering of Weather Data')
+plt.xlabel('Humidity')
+plt.ylabel('Temperature')
+plt.show()
