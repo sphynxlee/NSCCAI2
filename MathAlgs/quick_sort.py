061d1be3a5fb7132ae4df2146cfb2dd4a12ce210
haidun.lhd <kaiton.ri@gmail.com>
2024-01-23 09:16:55 -0400

    Add quick sort algorithm implementation

---
+++ MathAlgs/quick_sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot] # Sub-array of all elements less than the pivot
+        greater = [i for i in arr[1:] if i > pivot] # Sub-array of all elements greater than the pivot
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+arr = [10, 5, 2, 3]
+print(quick_sort(arr))
