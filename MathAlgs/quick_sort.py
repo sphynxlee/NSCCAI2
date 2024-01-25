a629d38b0fb98ea33c354a4df5dcfd475e6a7cc0
haidun.lhd <kaiton.ri@gmail.com>
2024-01-25 00:20:18 -0400

    Refactor quick_sort function to use a for loop for partitioning

--- MathAlgs/quick_sort.py
+++ MathAlgs/quick_sort.py
@@ -1,10 +1,17 @@
 def quick_sort(arr):
-    if len(arr) < 2:
+    if len(arr) <= 1:
         return arr
     else:
         pivot = arr[0]
-        less = [i for i in arr[1:] if i <= pivot] # Sub-array of all elements less than the pivot
-        greater = [i for i in arr[1:] if i > pivot] # Sub-array of all elements greater than the pivot
+        less = []
+        greater = []
+
+        for x in arr[1:]:
+            if x <= pivot:
+                less.append(x)
+            else:
+                greater.append(x)
+
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
 arr = [10, 5, 2, 3]
