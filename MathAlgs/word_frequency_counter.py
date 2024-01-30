73e47cfe88c2171c16fb0addbea9f70c481fd1c6
haidun.lhd <kaiton.ri@gmail.com>
2024-01-30 10:03:07 -0400

    Add word_frequency_counter2 function to calculate word frequencies

--- MathAlgs/word_frequency_counter.py
+++ MathAlgs/word_frequency_counter.py
@@ -12,3 +12,30 @@ text = "This is a practice problem. Practice makes perfect"
 
 result = word_frequency_counter(text)
 print(result)
+
+
+
+import string
+
+def word_frequency_counter2 (text):
+    output = {}
+
+    print(f'string.punctuation: {string.punctuation}')
+    punc_drop_translator = str.maketrans('', '', string.punctuation)
+    print(f'punc_drop_translator: {punc_drop_translator}')
+    clean_text = text.translate(punc_drop_translator)
+    print(f'clean_text: {clean_text}')
+
+    words = clean_text.split()
+    print(f'words: {words}')
+
+    for word in words:
+        if word in output:
+            output[word] += 1
+        else:
+            output[word] = 1
+
+    return output
+
+word_frequency_counter2("This is a practice problem. Practice makes perfect")
+
