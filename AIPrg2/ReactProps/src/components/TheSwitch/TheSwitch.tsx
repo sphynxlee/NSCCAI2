cfd63c5059fc624077ad5629a2d41d84949e9d00
haidun.lhd <kaiton.ri@gmail.com>
2024-02-07 10:03:20 -0400

    Add TheSwitch component and update page title

---
+++ AIPrg2/ReactProps/src/components/TheSwitch/TheSwitch.tsx
@@ -0,0 +1,11 @@
+import './TheSwitch.css'
+
+function TheSwitch(): JSX.Element{
+    return (
+        <>
+            <button className='my_btn'>Click me</button>
+        </>
+    );
+}
+
+export default TheSwitch;
