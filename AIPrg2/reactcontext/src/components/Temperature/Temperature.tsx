67fc6f518c5b79090f318b133ba8af44b1eb8760
haidun.lhd <kaiton.ri@gmail.com>
2024-02-21 10:37:09 -0400

    Add ThemeContext and useContext hook to Temperature component

--- AIPrg2/reactcontext/src/components/Temperature/Temperature.tsx
+++ AIPrg2/reactcontext/src/components/Temperature/Temperature.tsx
@@ -3,17 +3,22 @@
 // import { useState } from "react";
 import "./Temperature.css";
 import { ThemeProps } from "../interface";
+import { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 
 export default function Temperature(props: ThemeProps): JSX.Element {
 
+    const theme = useContext(ThemeContext);
     return (
         <>
             <h2>Temperature Component</h2>
             <p>The theme is: {props.theme}</p>
+            <p>The theme is: {theme}</p>
         </>
     );
 
 }
 
 // export {Temperature}
+// export default Temperature;
---
+++ AIPrg2/reactcontext/src/contexts/ThemeContext.ts
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// const [theme, setTheme] = useState("commodore 64");
+// export const ThemeContext = createContext([theme, setTheme]);
+
+export const ThemeContext = createContext("gibberish");
