4b661afb40dca113f3636d815db5610803794077
haidun.lhd <kaiton.ri@gmail.com>
2024-02-22 09:49:14 -0400

    Refactor graph initialization and add_edge method

--- MathAlgs/grahp/graph.py
+++ MathAlgs/grahp/graph.py
@@ -9,14 +9,22 @@
 
 class Graph:
     def __init__(self, num_nodes) -> None:
-        self.num_nodes = num_nodes
-        self.adjacency_matrix = [[False for _ in range(num_nodes)] for _ in range(num_nodes)]
+        # self.num_nodes = num_nodes
+        # self.adjacency_matrix = [[False for _ in range(num_nodes)] for _ in range(num_nodes)]
+
+        self.adjacency_matrix = []
+        for i in range(num_nodes):
+            tmp = []
+            for j in range(num_nodes):
+                tmp.append(False)
+            self.adjacency_matrix.append(tmp)
 
     def add_edge(self, node1, node2) -> None:
         self.adjacency_matrix[node1][node2] = True
         self.adjacency_matrix[node2][node1] = True
 
     def display_graph(self) -> None:
+        # print(self.adjacency_matrix)
         for row in self.adjacency_matrix:
             print(row)
 
