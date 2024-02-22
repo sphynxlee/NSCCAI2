cd8518db079d4ec2dc6ddfcd5eefc9aad2f47a2b
haidun.lhd <kaiton.ri@gmail.com>
2024-02-22 09:42:37 -0400

    Add Graph class implementation with adjacency matrix

---
+++ MathAlgs/grahp/graph.py
@@ -0,0 +1,31 @@
+# adjacency_matrix = [
+#     [False, True, False, False, False],
+#     [False, False, True, False, False],
+#     [False, False, False, True, False],
+#     [False, False, False, False, True],
+#     [True, False, False, False, False]
+# ]
+
+
+class Graph:
+    def __init__(self, num_nodes) -> None:
+        self.num_nodes = num_nodes
+        self.adjacency_matrix = [[False for _ in range(num_nodes)] for _ in range(num_nodes)]
+
+    def add_edge(self, node1, node2) -> None:
+        self.adjacency_matrix[node1][node2] = True
+        self.adjacency_matrix[node2][node1] = True
+
+    def display_graph(self) -> None:
+        for row in self.adjacency_matrix:
+            print(row)
+
+
+num_nodes = 5
+graph = Graph(num_nodes)
+graph.add_edge(0, 1)
+graph.add_edge(1, 2)
+graph.add_edge(2, 3)
+graph.add_edge(3, 4)
+graph.add_edge(4, 0)
+graph.display_graph()
