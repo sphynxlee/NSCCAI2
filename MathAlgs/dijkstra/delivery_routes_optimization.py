7d94255ae367f7dc82c24b02636f137350f96e38
haidun.lhd <kaiton.ri@gmail.com>
2024-02-27 10:15:07 -0400

    Add file path variable and update file reading logic

--- MathAlgs/dijkstra/delivery_routes_optimization.py
+++ MathAlgs/dijkstra/delivery_routes_optimization.py
@@ -1,6 +1,11 @@
+import os
+file_path = os.getcwd() + '/NSCCAI2/MathAlgs/dijkstra/city_info.txt'
+
 def read_city_info ():
     city_info = []
-    with open('city_info.txt', 'r') as file:
+    with open(file_path, 'r') as file:
         for line in file:
             city_info.append(line.strip().split(','))
     return city_info
+
+print(read_city_info())
