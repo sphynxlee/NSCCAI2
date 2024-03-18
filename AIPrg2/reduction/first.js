cd259c2525849c27de06537c4a3b6cf64f3925ca
haidun.lhd <kaiton.ri@gmail.com>
2024-03-18 11:40:19 -0300

    Add reducer function to first.js

---
+++ AIPrg2/reduction/first.js
@@ -0,0 +1,20 @@
+// console.log('hello world');
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// nums.reduce((acc, num) => {
+//     console.log(acc, num);
+//     return acc + num;
+// }, 0);
+
+
+initial_value = 0;
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+print = console.log;
+print(nums.reduce(reducer, initial_value));
+
+function reducer(accumulator, current_value) {
+    print(accumulator, current_value);
+    return accumulator + current_value;
+}
