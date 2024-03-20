a4a9d078da4ffbd9386b0e033a7235c9a27165fb
haidun.lhd <kaiton.ri@gmail.com>
2024-03-20 09:59:17 -0300

    Add reducer functions to count names and create an object of students

--- AIPrg2/reduction/count.js
+++ AIPrg2/reduction/count.js
@@ -1,5 +1,5 @@
 const names = ['Alice', 'Bob', 'Alice', 'Charlie', 'Bob', 'Alice'];
-
+// { Alice: 3, Bob: 2, Charlie: 1 }
 function reducer(accumulator, current_value) {
     if (accumulator[current_value]) {
         accumulator[current_value]++;
--- AIPrg2/reduction/obj.js
+++ AIPrg2/reduction/obj.js
@@ -3,7 +3,11 @@ const students = {
     "student2": {id: 2, "name": "Jane", "age": 21},
     "student3": {id: 3, "name": "Bob", "age": 22},
 };
-
+// result: {
+//     '1': { id: 1, name: 'John', age: 20 },
+//     '2': { id: 2, name: 'Jane', age: 21 },
+//     '3': { id: 3, name: 'Bob', age: 22 }
+//   }
 function reducer(accumulator, current_value) {
     console.log("accumulator:", accumulator);
     console.log("current_value:", current_value);
