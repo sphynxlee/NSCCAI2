edb61123b152a0915cab845f0ffdd867d4e4bf5a
haidun.lhd <kaiton.ri@gmail.com>
2024-03-20 09:58:16 -0300

    Add reducer function to reduce students object into a result object

---
+++ AIPrg2/reduction/obj.js
@@ -0,0 +1,17 @@
+const students = {
+    "student1": {id: 1, "name": "John", "age": 20},
+    "student2": {id: 2, "name": "Jane", "age": 21},
+    "student3": {id: 3, "name": "Bob", "age": 22},
+};
+
+function reducer(accumulator, current_value) {
+    console.log("accumulator:", accumulator);
+    console.log("current_value:", current_value);
+    accumulator[students[current_value].id] = students[current_value];
+    return accumulator;
+}
+
+initial_value = {};
+
+const result = Object.keys(students).reduce(reducer, initial_value);
+console.log("result:", result);
