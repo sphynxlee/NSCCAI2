da53fcfadec63388ba84594d3a86ad15abaf67a9
haidun.lhd <kaiton.ri@gmail.com>
2024-03-22 01:22:22 -0300

    Add solution for removing duplicates from sorted array with at most two duplicates

---
+++ MathAlgs/leetCode/medium/80_removeDuplicates.py
@@ -0,0 +1,13 @@
+# Input: nums = [1,1,1,2,2,3]
+# Output: 5, nums = [1,1,2,2,3,_]
+
+def removeDuplicates(nums):
+    i = 0
+    for j in range(len(nums)):
+        if i < 2 or nums[j] != nums[i-2]:
+            nums[i] = nums[j]
+            i += 1
+    return (i, nums[:i])
+
+nums = [1,1,1,2,2,3]
+print(removeDuplicates(nums))
