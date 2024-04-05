f61315e4913a87d5b9e0eba832a7fef242644f72
haidun.lhd <kaiton.ri@gmail.com>
2024-04-04 23:57:29 -0300

    Update XGBoost reference link in xgboost_test.py

--- ML/XGBoost/xgboost_test.py
+++ ML/XGBoost/xgboost_test.py
@@ -2,7 +2,7 @@
 # It's widely used for supervised learning problems, particularly in structured/tabular data,
 # and has proven to be highly effective in various machine learning competitions and real-world applications.
 
-## reference: https://www.datacamp.com/tutorial/xgboost-in-python
+# reference: https://www.datacamp.com/tutorial/xgboost-in-python
 
 import numpy as np
 import xgboost as xgb
